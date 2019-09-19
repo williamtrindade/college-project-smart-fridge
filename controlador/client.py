5a73b721ac38c3cd48bc7798e83d7fe0a560545a
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-09-19 11:55:52 -0300

    Cliente e servidor com funcionalidades implementadas

---
+++ controlador/client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import socket
+import argparse
+
+HOST = 'localhost'
+
+
+def enviar_msg_tcp(msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, 3000))
+    s.send(msg.encode())
+    print(s.recv(4096))
+
+
+def enviar_msg_udp(msg):
+    s = socket.socket(socket.AF_INET,
+                      socket.SOCK_DGRAM)
+    s.sendto(msg.encode(), (HOST, 3030))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('modo', choices=('tcp', 'udp'),
+                        default='tcp', nargs='?')
+    parser.add_argument('msg', type=str)
+    args = parser.parse_args()
+
+    if args.modo == 'tcp':
+        enviar_msg_tcp(args.msg)
+    else:
+        enviar_msg_udp(args.msg)
+
+
+if __name__ == '__main__':
+    main()
--- controlador/server.py
+++ controlador/server.py
@@ -3,6 +3,9 @@
 """Servidor tcp e udp"""
 
 import socket
+import re
+import sqlite3
+import sys
 from threading import Thread
 
 LISTEN_ADDR = 'localhost'
@@ -20,7 +23,6 @@ def tcp_listener():
     try:
         while True:
             conn, address = s.accept()
-            address
             Thread(target=tcp_handler, args=(conn, address)).start()
     finally:
         s.close()
@@ -28,10 +30,9 @@ def tcp_listener():
 
 def tcp_handler(conn, address):
     print("Conexão TCP de: ", address)
-    data = conn.recv(1024)
-    print("Mesage: ", data)
-    conn.send(b"OK")
-    conn.close()
+    msg = conn.recv(1024).decode()
+    ret = client_handler(msg)
+    conn.send(ret.encode())
 
 
 def udp_listener():
@@ -49,7 +50,66 @@ def udp_listener():
 
 def udp_handler(data, address):
     print("Mensagem UDP de: ", address)
-    print("Message: ", data.decode("utf-8"))
+    client_handler(data.decode())
+
+
+def client_handler(msg):
+    op, prod = re.split(r' ', msg)
+    with Product() as p:
+        if op == 'STOR' or op == 'RETR':
+            print("OP: {} PRODUTO: {}".format(op, prod))
+            p.save(op, prod)
+            return 'ok'
+        else:
+            return 'Operação invalida'
+
+
+class Product():
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.conn.close()
+
+    def save(self, op, name):
+        """Registra uma transação, se o produto existe, sua quantidade é
+        alterada, caso contrario o produto é registrado"""
+
+        c = self.conn.cursor()
+        print(name)
+        if self.exists(name):
+            prod_id = self.findId(name)
+            c.execute('UPDATE product SET qnt=qnt {} 1 WHERE id=?'
+                      .format('+' if op == 'STOR' else '-'),
+                      [prod_id])
+        else:
+            c.execute('INSERT INTO product(name, qnt) VALUES (?, 1)', [name])
+            prod_id = self.findId(name)
+
+        c.execute("""INSERT INTO transactions(id_product, date_time, opcode)
+        VALUES (?, datetime('now'), ?)""", [prod_id, op])
+        c.close()
+        self.conn.commit()
+
+    def exists(self, name):
+        """ Verifica se existe um produto com o nome igual a `name` """
+
+        c = self.conn.cursor()
+        c.execute('SELECT rowid FROM product WHERE name=?',
+                  [name])
+        d = c.fetchall()
+        print("data = {}".format(d))
+        return len(d) != 0
+
+    def findId(self, name):
+        """ Retorna o id do produto com nome igual a `name` """
+
+        c = self.conn.cursor()
+        c.execute('SELECT p.id FROM product p WHERE name=?', [name])
+        return c.fetchone()[0]
 
 
 def main():
@@ -63,7 +123,7 @@ def main():
         tcp_server.join()
         udp_server.join()
     except KeyboardInterrupt:
-        exit(0)
+        sys.exit()
 
 
 if __name__ == '__main__':
